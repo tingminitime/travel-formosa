40c3035065fc6c91391eb41856848c4b55a6441d
tingminitime <e0610k@gmail.com>
2021-11-11 18:40:44 +0800

    新增content內容

--- module/swiper.js
+++ module/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper_header = new Swiper('.swiper-header', {
   speed: 800,
   spaceBetween: 100,
   grabCursor: true,
@@ -13,8 +13,35 @@ const swiper = new Swiper('.swiper', {
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-header',
   },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+const swiper_hotSpot = new Swiper('.swiper-hotSpot', {
+  speed: 400,
+  spaceBetween: 12,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  breakpoints: {
+    320: {
+      spaceBetween: 12
+    },
+    767: {
+      spaceBetween: 12
+    },
+    992: {
+      spaceBetween: 24
+    },
+  },
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
 
   // Navigation arrows
   navigation: {
